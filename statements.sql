594baa60db75e3f499baa5551e0b1c6c5c6c3242
DarthMax <max@kopfueber.org>
2015-05-21 13:11:29 +0200

    added sql statements to calculate z-score

--- statements.sql
+++ statements.sql
@@ -1,4 +1,6 @@
 
+-- in deu_news_2014
+
 -- Tabelle für vorberechnete Werte
 CREATE TABLE aliss15a_words (
     w_id INT(10) UNSIGNED PRIMARY KEY,
@@ -6,3 +8,60 @@ CREATE TABLE aliss15a_words (
     mean FLOAT,
     standard_derivation FLOAT
 );
+
+-- Tablle für vorberechnete relative Häufigkeiten
+CREATE TABLE aliss15a_daily_words (
+    w_id INT(10) UNSIGNED,
+    date date,
+    relative_freq FLOAT,
+    z_score FLOAT,
+    tf_idf FLOAT,
+    poisson FLOAT,
+    PRIMARY KEY(w_id, date)
+);
+
+
+
+-- Berechne Relative Wortfrequenz pro Wort und Tag (7min für 2014)
+
+INSERT INTO aliss15a_daily_words (
+    SELECT w_id, daily_words.date, (freq/daily_sums.sum) 
+        FROM daily_words 
+            JOIN (SELECT date, SUM(freq) as sum FROM daily_words GROUP BY date) AS daily_sums ON daily_sums.date=daily_words.date
+);
+
+
+-- Berechne Erwartungswert und Standardabweichung pro Wort (15 min für 2014)
+INSERT INTO aliss15a_words (w_id,mean,standard_derivation) (
+    SELECT w_id, AVG(relative_freq), STD(relative_freq) 
+        FROM aliss15a_daily_words 
+        GROUP BY w_id
+);
+
+
+-- Berechne Dokumentenhäufigkeit pro Wort (Dokument = 1 Tag)
+INSERT INTO aliss15a_words (w_id,relative_document_frequency) ( 
+    SELECT daily_words.w_id, (word_counts.wc/COUNT(DISTINCT date)) 
+        FROM daily_words
+        JOIN (
+            SELECT w_id, COUNT(DISTINCT date) as wc 
+                FROM daily_words
+                GROUP BY w_id
+        ) AS word_counts ON word_counts.w_id=daily_words.w_id
+) ON DUPLICATE KEY UPDATE relative_document_frequency=values(relative_document_frequency);
+
+
+
+-- Calculate Z-Score pro Wort und Tag für 2015
+INSERT INTO aliss15a_daily_words (w_id, date, zscore) (
+    SELECT (now.w_id, now.date, 
+            (now.relative_freq - ref-aliss.mean) / ref_aliss.standard_derivation)  --zscore
+        FROM aliss15a_daily_words as now
+        JOIN words as now_words on now_words.w_id = now.w_id
+        JOIN deu_news_2014.words as ref on now_words.word = ref.word
+        JOIN deu_news_2014.aliss15a_words as ref_aliss on ref.w_id = ref_aliss.w_id
+        WHERE now.w_id = 1 AND now.date=DATE('2014-07-10'); 
+);
+
+
+where date=DATE('2014-07-10')
