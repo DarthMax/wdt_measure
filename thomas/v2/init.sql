f9c71d297286eff22bb6846a5a03162e67e6171c
karlkurzke <erna@privathost.de>
2015-06-14 23:59:43 +0200

    ok

--- thomas/v2/init.sql
+++ thomas/v2/init.sql
@@ -1,1 +1,6 @@
-alter table alis15a_daily_words add lin_filt_freq float;
+alter table aliss15a_windows_of_r add lin_filt_freq float;
+
+update aliss15a_windows_of_r r
+inner join daily_words dw 
+on r.w_id=dw.w_id and r.date = dw.date
+set lin_filt_freq=r.window8/dw.freq;
