fb136ffe43ef87f5bdbd14ba0b1a3d460fade190
karlkurzke <erna@privathost.de>
2015-06-16 14:29:40 +0200

    updated

--- R/compare_ranking.r
+++ R/compare_ranking.r
@@ -50,7 +50,7 @@ test_multiple_average_overlap()
 
 
 ## Test on real data
-
+dbGetQuery(mydb, "SET NAMES 'utf8'") #utf-8
 
 get_measures_from_day <- function (date, measure, limit) {
   query = paste("SELECT * ",
@@ -68,15 +68,65 @@ get_measures_from_day <- function (date, measure, limit) {
   fetch(response, n=-1)
 }
 
+get_z_value<- function() {
+  query <- "SELECT
+now.z_score,
+now_daily_words.freq,
+now.relative_freq,
+ref_aliss.mean,
+ref_aliss.standard_derivation,
+ref_aliss.relative_document_frequency,
+ref.word,
+now_words.word
+FROM aliss15a_daily_words as now
+JOIN daily_words as now_daily_words on now.w_id = now_daily_words.w_id and now.date = now_daily_words.date
+JOIN words as now_words on now_words.w_id = now.w_id
+JOIN deu_news_2014.words as ref on now_words.word = ref.word
+JOIN deu_news_2014.aliss15a_words as ref_aliss on ref.w_id = ref_aliss.w_id
+WHERE now.date=Date('2015-03-25') AND ((now.relative_freq > (@threshold*10) AND ref_aliss.relative_document_frequency < 0.1) OR ref_aliss.relative_document_frequency >= 0.1)
+ORDER BY now.z_score desc
+LIMIT 100;"
+  response = dbSendQuery(mydb, query)
+  fetch(response, n=-1)
+}
+
+get_relwindow <- function() {
+  query <- "select words.word, daily.date, daily.freq,win.window8, daily.freq/win.window8 relWindow From aliss15a_windows_of_r win JOIN words ON win.w_id=words.w_id JOIN daily_words daily ON daily.w_id=win.w_id AND daily.date=win.date WHERE win.date=Date('2015-03-25') AND daily.date=('2015-03-25') AND daily.freq>100 ORDER BY relWindow DESC limit 100;"
+  response = dbSendQuery(mydb, query)
+  fetch(response, n=-1)
+ }
+1/365
 # Clear Resultset
 # dbClearResult(dbListResults(mydb)[[1]])
-day <- "2015-05-01"
-tf_idf <- get_measures_from_day(day, "dw.tf_idf", "1000")
-poisson <- get_measures_from_day(day, "dw.poisson", "1000")
-poisson_old <- get_measures_from_day(day, "daily_words.poisson", "1000")
-freqratio <- get_measures_from_day(day, "dw.freqratio", "1000")
-freqratio_old <- get_measures_from_day(day, "daily_words.freqratio", "1000")
-z_score <- get_measures_from_day(day, "dw.z_score", "1000")
+day <- "2015-03-25"
+tf_idf <- get_measures_from_day(day, "dw.tf_idf", "100")
+poisson <- get_measures_from_day(day, "dw.poisson", "100")
+poisson_old <- get_measures_from_day(day, "daily_words.poisson", "100")
+freqratio <- get_measures_from_day(day, "dw.freqratio", "100")
+freqratio_old <- get_measures_from_day(day, "daily_words.freqratio", "100")
+lin_filt_freq <- get_measures_from_day(day, "lin_filt_freq", "100")
+z_score <- get_z_value()
+relWindow <- get_relwindow()
+
+allInOne <- as.data.frame(poisson[,c("word")])
+colnames(allInOne) <- c("poisson")
+allInOne$tf_idf <- tf_idf[,c("word")]
+allInOne$freqratio <- freqratio[,c("word")]
+allInOne$z_score <- z_score[,c("word")]
+
+z_score[1:20,c("word","freq")]
+relWindow[1:20,c("word","freq")]
+
+
+library(xtable)
+xtable(allInOne[1:10,1:2], digits=2)
+xtable(allInOne[1:10,3:4], digits=2)
+xtable(allInOne[31:40,1:2], digits=2)
+xtable(allInOne[31:40,3:4], digits=2)
+
+??xtable
+
+
 
 #versuchSQL <- "SELECT * FROM aliss15a_daily_words dw  JOIN words ON dw.w_id=words.w_id  WHERE dw.date=Date('15-04-01') ORDER BY dw.tf_idf DESC LIMIT 100"
 #versuch2SQL <- "SELECT * FROM (SELECT * FROM aliss15a_daily_words WHERE date=Date( '15-03-01')) dw  JOIN words ON dw.w_id=words.w_id ORDER BY dw.tf_idf DESC LIMIT 100"
@@ -85,16 +135,18 @@ z_score <- get_measures_from_day(day, "dw.z_score", "1000")
 
 
 #freqratio <- get_measures_from_day("2015-05-01", "freqratio", "10")
-all_meassures <- cbind(tf_idf$word, poisson$word, z_score$word, freqratio$word, freqratio_old$word, poisson_old$word)
+all_meassures <- cbind(tf_idf$word, poisson$word, z_score$word, freqratio$word)
+#all_meassures <- cbind(poisson$word,freqratio$word, freqratio_old$word, poisson_old$word)
 average_overlap_data <- multiple_average_overlap(all_meassures)
 #Format data in data frame
 average_overlap_data <- as.data.frame(average_overlap_data)
 colnames(average_overlap_data) <- c("List","List_to_compare", "average_overlap")
 average_overlap_data$List <- as.factor(average_overlap_data$List)
-levels(average_overlap_data$List) <- c("tf_idf", "poisson", "z-score", "freqratio","freqratio_old", "poisson_old" )
+levels(average_overlap_data$List) <- c("tf-idf", "z-score","poisson", "freqratio" )
+#levels(average_overlap_data$List) <- c("tf_idf","poisson", "z-score", "freqratio")
 average_overlap_data$List_to_compare <- as.factor(average_overlap_data$List_to_compare)
-levels(average_overlap_data$List_to_compare) <- c( "poisson", "z-score", "freqratio","freqratio_old", "poisson_old")
-
+#levels(average_overlap_data$List_to_compare) <- c("freqratio","freqratio_old", "poisson_old")
+levels(average_overlap_data$List_to_compare) <- c( "z-score","poisson", "freqratio")
 #result
 average_overlap_data
 
